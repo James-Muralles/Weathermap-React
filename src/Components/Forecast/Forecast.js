15bb4269e1111a74bfa642857c5307f6f472f98a
James Muralles <james.muralles23@gmail.com>
2020-07-20 15:36:26 -0500

    got dark sky working

--- src/Components/Forecast/Forecast.js
+++ src/Components/Forecast/Forecast.js
@@ -1,13 +1,16 @@
 import React from "react";
+import DarkSky from "../../util/DarkSky";
 
 
 class Forecast extends React.Component {
     render(){
         return (
             <div id="forecast-cards"
-                 className="row text-center sticky-top rounded mb-auto d-flex justify-content-center">
+                 className="text-center sticky-top rounded mb-auto d-flex justify-content-center">
 
-                <div className="col-3 rounded day-1" id="day-1">Day 1</div>
+                <div className="col-3 rounded day-1" id="day-1">
+                    <DarkSky/>
+                </div>
                 <div className="col-3 rounded day-2" id="day-2">Day 2</div>
                 <div className="col-3 rounded day-3" id="day-3">Day 3</div>
 
--- src/util/DarkSky.js
+++ src/util/DarkSky.js
@@ -1,22 +1,77 @@
 import React from "react";
+import fetch from "node-fetch";
+// let fetch = require('node-fetch');
 
+const darksky = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/";
+const key = 'a8197a0c91a20e20df9850290ed6b6f5';
+const lat = '45';
+const lng = '-75';
+const uri = darksky + key + '/' + lat + ',' + lng;
 
-class DarkSky extends React.Component{
-    getWeather(){
-        fetch("https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyKey + "/" + marker.getLngLat().lat + "," + marker.getLngLat().lng).done(function (data) {
-            console.log(data);
-            console.log(new Date(data.currently.time * 1000));
-            console.log(data.daily.data[0].apparentTemperatureHigh);
-            console.log(data.currently.icon);
+const options = {
+    method: 'GET',
+    mode: 'cors'
+};
+const req = new fetch.Request(uri, options);
 
-            reverseGeocode({
-                    lat: marker.getLngLat().lat,
-                    lng: marker.getLngLat().lng
-                },
-                mapboxgl.accessToken).then(function(results){
-                console.log(results);
-                $('h1').html(results )
-            });
+class DarkSky extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentWeather: [''],
+        };
+        this.getWeather = this.getWeather.bind(this)
+
     }
+
+    getWeather() {
+        fetch(req)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json();
+                } else {
+                    throw new Error('Bad HTTP!')
+                }
+
+            }).then((j) => {
+            console.log(j.currently.temperature);
+            this.setState({currentWeather: j.currently.temperature})
+
+
+        }).catch((err) => {
+            console.log('Error:', err.message)
+        });
+    }
+
+    // weather(){
+    //     fetch(req)
+    //         .then((res) =>{
+    //             console.log(res.json())
+    //         }).then((data) => {
+    //             console.log(data.currently.temperature);
+    //         this.setState({currentWeather: data.currently.temperature})
+    //
+    //     })
+    //
+    //
+    // }
+    componentDidMount() {
+        this.getWeather()
+    }
+
+    render(){
+
+
+        return (
+            <h1>
+                {this.state.currentWeather}
+
+            </h1>
+        )
+    }
+
+
 }
-}
+export default DarkSky;
+
+
